c056369bc0019729faf92225504c3c196b18e8cb
Paula Bianca Menezes dos Santos <heybbyanca@gmail.com>
2025-09-25 19:50:35 -0300

    Realizando conectividade com Neon

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,8 +1,35 @@
-const { Sequelize } = require ('sequelize');
+import { Sequelize } from "sequelize";
+import Artist from "../models/artistsModel";
+import dotenv from "dotenv";
+dotenv.config();
 
-const sequelize = new Sequelize('postgres', 'postgres', '123456', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+// URL fornecida pelo Neon
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://neondb_owner:npg_SnUqBr7DK2xj@ep-odd-unit-acxdd8oc-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  logging: console.log, // mostra queries no terminal
+});
+
+export const dbConnected = (async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Conexão com Neon bem-sucedida!");
+  } catch (error) {
+    console.error("❌ Não foi possível conectar:", error);
+  }
+})();
+
+export const tableCreated = (async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Conectado ao Neon!");
+
+    await Artist.sync({ alter: true }); // cria ou atualiza tabela
+    console.log("✅ Tabela Artist criada ou atualizada!");
+  } catch (err) {
+    console.error("❌ Erro ao conectar/criar tabela:", err);
+  }
+})();
 
 export default sequelize;
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const PORT = 3333;
 app.use(express.json());
 app.use(logger);
 app.use("/artists", artistRoutes);
+
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
